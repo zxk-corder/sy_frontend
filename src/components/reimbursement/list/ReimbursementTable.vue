061b06dbf9a6ae82388a77babce7874e4ca7c3ad
zxk <3173497068@qq.com>
2026-06-02 15:55:03 +0800

    feat:列表样式修改

--- src/components/reimbursement/list/ReimbursementTable.vue
+++ src/components/reimbursement/list/ReimbursementTable.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="table-wrap">
-    <el-table v-loading="loading" :data="data" border stripe>
-      <el-table-column label="" width="50" fixed="left" align="center">
+    <el-table
+      v-loading="loading"
+      :data="data"
+      border
+      stripe
+      size="small"
+      table-layout="fixed"
+      class="list-table"
+    >
+      <el-table-column label="" width="44" align="center">
         <template #header>
           <div class="index-list-icon-header">
             <img :src="listIcon" alt="列表" class="index-list-icon" />
@@ -11,7 +19,7 @@
           {{ indexMethod($index) }}
         </template>
       </el-table-column>
-      <el-table-column label="操作" width="120" fixed="left" align="center">
+      <el-table-column label="操作" width="96" align="center">
         <template #default="{ row }: { row: ReimbursementListItem }">
           <ListRowActions
             :editable="canSubmitReimbursement(row.status)"
@@ -24,47 +32,72 @@
           />
         </template>
       </el-table-column>
-      <el-table-column label="报销单号" min-width="160">
+      <el-table-column label="报销单号" width="158" show-overflow-tooltip>
         <template #default="{ row }">
-          <el-button link type="primary" @click="emit('view', row.id)">
+          <el-button link type="primary" class="cell-link" @click="emit('view', row.id)">
             {{ row.reimNo }}
           </el-button>
         </template>
       </el-table-column>
-      <el-table-column label="单据状态" width="100">
+      <el-table-column label="单据状态" width="76" align="center">
         <template #default="{ row }: { row: ReimbursementListItem }">
           <el-tag :type="statusMeta(row.status).type" size="small">
             {{ statusMeta(row.status).label }}
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="documentType" label="单据类型" width="120" />
-      <el-table-column label="报销人" min-width="150">
+      <el-table-column
+        prop="documentType"
+        label="单据类型"
+        width="92"
+        show-overflow-tooltip
+      />
+      <el-table-column label="报销人" min-width="62" show-overflow-tooltip>
         <template #default="{ row }">
           {{ formatListReimburser(row.reimburserName, row.reimburserNo) }}
         </template>
       </el-table-column>
-      <el-table-column label="报销部门" min-width="180">
+      <el-table-column label="报销部门" min-width="80" show-overflow-tooltip>
         <template #default="{ row }">
           {{ formatListDepartment(row.reimDepartmentName, row.reimDepartmentNo) }}
         </template>
       </el-table-column>
-      <el-table-column prop="reimCompanyName" label="费用归属公司" min-width="160" />
-      <el-table-column prop="businessTypeName" label="业务类型" min-width="120" />
-      <el-table-column label="报销标题" min-width="180">
+      <el-table-column
+        prop="reimCompanyName"
+        label="费用归属公司"
+        min-width="88"
+        show-overflow-tooltip
+      />
+      <el-table-column
+        prop="businessTypeName"
+        label="业务类型"
+        width="80"
+        show-overflow-tooltip
+      />
+      <el-table-column label="报销标题" min-width="96" show-overflow-tooltip>
         <template #default="{ row }">
-          <el-button link type="primary" @click="emit('view', row.id)">
+          <el-button link type="primary" class="cell-link" @click="emit('view', row.id)">
             {{ row.title }}
           </el-button>
         </template>
       </el-table-column>
-      <el-table-column prop="reason" label="报销事由" min-width="160" show-overflow-tooltip />
-      <el-table-column label="补助金额" width="120" align="right">
+      <el-table-column
+        prop="reason"
+        label="报销事由"
+        min-width="88"
+        show-overflow-tooltip
+      />
+      <el-table-column label="补助金额" width="96" align="right">
         <template #default="{ row }">
           <AmountText :value="row.subsidyAmount" />
         </template>
       </el-table-column>
-      <el-table-column prop="createdAt" label="创建时间" width="120" />
+      <el-table-column
+        prop="createdAt"
+        label="创建时间"
+        width="108"
+        show-overflow-tooltip
+      />
     </el-table>
 
     <div class="pagination-wrap">
@@ -138,6 +171,35 @@ function indexMethod(index: number) {
   background: #fff;
   padding: 16px;
   border-radius: 4px;
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.list-table {
+  width: 100%;
+}
+
+.list-table :deep(.el-table__cell .cell) {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.list-table :deep(.cell-link) {
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
+}
+
+.list-table :deep(.cell-link > span) {
+  display: inline-block;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
 }
 
 .pagination-wrap {
